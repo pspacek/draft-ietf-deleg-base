40f44cc2a2bdbae8d9194d0ad3c5517dd3e43318
Petr Špaček <pspacek@isc.org>
2026-03-27 16:37:26 +0100

    Shell hack to simplify PATH setup

--- testvec/rerun.sh
+++ testvec/rerun.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 set -o nounset -o errexit -o xtrace
-export PATH=~/w/pkg/bind/install/each-deleg-auth_36dd69fac375b546870915f58c13edc6f91add58_ccache_gcc-march-native-ggdb3-O0/bin:~/w/pkg/bind/install/each-deleg-auth_36dd69fac375b546870915f58c13edc6f91add58_ccache_gcc-march-native-ggdb3-O0/sbin:$PATH
-export PYTHONPATH=~/w/pkg/python-dns/git
+source testvars.sh
 
 rm -rf deleg/
 ~/w/pkg/respdiff/git/qprep.py -f tcpdns --tcpdns-file queries.tcpdns deleg/
---
+++ testvec/testvars.sh
@@ -0,0 +1,4 @@
+export PATH=~/w/pkg/bind/install/each-deleg-auth_07f78a37a4e59b6382d008e54b8d366b983ee76b_ccache_gcc-march-native-ggdb3-O0/bin:~/w/pkg/bind/install/each-deleg-auth_07f78a37a4e59b6382d008e54b8d366b983ee76b_ccache_gcc-march-native-ggdb3-O0/sbin:/home/pspacek/w/pkg/knot/git/src:$PATH
+export PYTHONPATH=~/w/pkg/python-dns/git
+ALLZONES="unsigned.test nsec.test nsec3.test optout-nsec3.test"
+SIGNEDZONES="nsec.test nsec3.test optout-nsec3.test"
